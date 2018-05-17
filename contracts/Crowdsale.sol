3a5375da2bb73a8037529d7f4eb720ed3674b22f
Sr Developer <dev.l33@zoho.com>
2018-05-17 12:59:32 +0900

    first commit

---
+++ contracts/Crowdsale.sol
@@ -0,0 +1,169 @@
+pragma solidity ^0.4.21;
+
+import "./ERC20.sol";
+import "./SafeMath.sol";
+import "./Ownable.sol";
+
+interface Manager {
+  function ethusd() external view returns (uint256);
+}
+
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale,
+ * allowing investors to purchase tokens with ether. This contract implements
+ * such functionality in its most fundamental form and can be extended to provide additional
+ * functionality and/or custom behavior.
+ * The external interface represents the basic interface for purchasing tokens, and conform
+ * the base architecture for crowdsales. They are *not* intended to be modified / overriden.
+ * The internal interface conforms the extensible and modifiable surface of crowdsales. Override
+ * the methods to add functionality. Consider using 'super' where appropiate to concatenate
+ * behavior.
+ */
+contract Crowdsale is Ownable {
+  using SafeMath for uint256;
+
+  // The token being sold
+  ERC20 public token;
+
+  // Manager contract for getting ETH price.
+  Manager public manager;
+
+  // Address where funds are collected
+  address public wallet;
+
+  // Amount of wei raised
+  uint256 public weiRaised;
+
+  // Amount of token sold out
+  uint256 public tokenSold;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+  /**
+   * @param _wallet Address where collected funds will be forwarded to
+   * @param _token Address of the token being sold
+   * @param _manager Address of manager smart contract
+   */
+  constructor(address _wallet, ERC20 _token, Manager _manager) public {
+    require(_wallet != address(0));
+    require(_token != address(0));
+    require(_manager != address(0));
+
+    wallet = _wallet;
+    token = _token;
+    manager = _manager;
+  }
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+    tokenSold = tokenSold.add(tokens);
+
+    _processPurchase(_beneficiary, tokens);
+    emit TokenPurchase(
+      msg.sender,
+      _beneficiary,
+      weiAmount,
+      tokens
+    );
+
+    _forwardFunds();
+  }
+
+  function price() public view returns(uint) {
+    // stage 1 6,000,000 coins
+    if (tokenSold < 6000000 ether ) {
+      return 57;
+    // stage 2 9,000,000 coins
+    } else if (tokenSold < 15000000 ether) {
+      return 72;
+    // stage 3 10,500,000
+    } else if (tokenSold < 25500000) {
+      return 86;
+    } else {
+      return 100;
+    }
+  }
+
+  function allocate(address _to, uint _amount) onlyOwner public {
+    _processPurchase(_to, _amount);
+  }
+
+  // -----------------------------------------
+  // Internal interface (extensible)
+  // -----------------------------------------
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use super to concatenate validations.
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0);
+  }
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    token.transfer(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Override to extend the way in which ether is converted to tokens.
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    return _weiAmount * manager.ethusd() / price();
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+}
